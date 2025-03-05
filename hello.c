0fe4d3fe8d1c0a7396bbe5eeb4a84132ca63dec9
xxl53668839 <xxl53668829@gmail.com>
2025-03-05 13:59:25 +0800

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main() {
+    printf("你好，陳泰文!\n");
+    return 0;
+}
---
+++ pi.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    float pi;
+    pi=3.14159;
+
+    scanf("%d",&a);
+    printf("%d\n",a*a*pi);
+    return 0;
+}
