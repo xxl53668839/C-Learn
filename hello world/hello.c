9a2b281cb9add63cae9356784c0ff141057be0da
xxl53668829 <xxl53668829@gmail.com>
2025-03-05 16:53:38 +0800

    新增 hello.c 和 pi.c 檔案，實現基本的輸出和圓面積計算功能

---
+++ hello world/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() 
+
+{
+    printf("你好，陳泰文!\n");
+    printf("Hello, World!\n");
+    printf("こんにちは、世界!\n");
+    
+    return 0;
+}
---
+++ pi/pi.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    float a,pi;
+    pi=3.14159;
+
+    scanf("%f",&a);
+    printf("%.2f\n",a*a*pi);
+    return 0;
+}
