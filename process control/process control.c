908e5fafc5e4ccdc829710dee22a8132fdfb88f3
xxl53668829 <xxl53668829@gmail.com>
2025-03-05 22:26:50 +0800

    修正 process_control.c 檔案的格式，優化輸入提示並調整奇偶數判斷邏輯

--- process control/process control.c
+++ process control/process control.c
@@ -1,26 +1,33 @@
 #include <stdio.h>
 
-int main() {
-    int num;
-    printf("請輸入一個數字 (輸入 0 結束):\n");
-    scanf("%d", &num); //輸入一個數字並存入 num
+int main()
 
-    while (num != 0)   //當 num 不等於 0 時執行迴圈
+    //輸入一個整數，判斷其為奇數或偶數。
+{
+
+int num;
+    printf("請輸入任意數字: \n");
+    scanf("%d",&num);
+
+    while(num!=0) //當num不等於0時進入迴圈
     {
-        if (num % 2 == 0) //判斷 num 是否為偶數
+        if (num%2==0) //如果num的餘數等於0時
         {
-            printf("%d 是偶數\n", num); //是偶數則印出
-        } 
-        else //否則為奇數
-        {
-            printf("%d 是奇數\n", num);  
+            printf("%d為偶數",num); //顯示為偶數
         }
-        printf("請輸入一個數字 (輸入 0 結束):\n"); 
-        scanf("%d", &num); //再次輸入一個數字並存入 num
+
+        else //反之
+        {
+            printf("%d為奇數",num); //顯示為奇數
+        }
+
+    printf("請輸入任意數字: \n");
+    printf("輸入0可退出程式\n");
+    scanf("%d",&num); //if或else執行完畢後，再次輸入數字
+
     }
 
-    printf("程式結束。\n");
-    return 0;
+    printf("程式結束\n");
+return 0;
+
 }
-
-
