7c47a3629c17ef359de82479b0696257aecca7d9
xxl53668829 <xxl53668829@gmail.com>
2025-03-07 23:42:32 +0800

    重構 CRC_check.c 檔案，簡化 CRC 校驗邏輯並新增用戶輸入功能；新增 XY.c 檔案，實現座標判斷功能

--- CRC_check/CRC_check.c
+++ CRC_check/CRC_check.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
-
-unsigned short CrcCheck(unsigned char *data, unsigned char length) {
-unsigned short crc = 0xFFFF;  // 初始 CRC 值
-    for (int i = 0; i < length; i++) {
-        crc ^= data[i];  // XOR 進行異或運算
-        for (int j = 0; j < 8; j++) {  // 處理每個 bit
-            if (crc & 0x0001) {
-                crc = (crc >> 1) ^ 0xA001;  // 多項式 0xA001
-            } else {
-                crc >>= 1;
-            }
-        }
+int main()
+{
+    unsigned short crc=0xFFFF;
+    unsigned short i;
+    unsigned short data;
+   
+   printf("請輸入16進制:");
+    scanf("%d\n",&data);
+   for (i=0; i<8; i++)
+    {
+        if ((data ^ crc) & 0x0001)
+            crc = (crc >> 1) ^ 0x8408;
+        else
+            crc = crc >> 1;
     }
-    return crc;
-}
-
-int main() {
-    unsigned char message[] = { 0x01, 0x03, 0x9C, 0x41, 0x00, 0x02 }; // Modbus 讀取指令
-    unsigned short crc = CrcCheck(message, 6);
-    printf("CRC-16: 0x%04X\n", crc);  // 輸出 CRC 校驗碼
-    return 0;
+    printf("%#x\n",crc);
+printf("程式結束\n");
+return 0;
 }
---
+++ XY/XY.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main()
+{
+    int X,Y;
+    int W=320;
+    int H=240;
+    int L=10,T=10,R=100,B=200;
+
+    while(1)//無限迴圈
+    {
+        printf("\n");
+        printf("請輸入X的座標:\n");
+        scanf("%d",&X); //輸入X座標
+
+        printf("請輸入Y的座標:\n");
+        scanf("%d",&Y); //輸入Y座標
+
+    if (X==0&&Y==0) //如果XY都等於0時
+        {
+            break; //跳出迴圈，結束程式
+        }
+
+    if((X>W)||(Y>H)) //如果X大於W或Y大於H時
+        {
+            printf("Err"); //顯示Err
+            continue;
+        }
+
+    if ((X>=L)&&(X<=R)&&(Y>=T)&&(Y<=B)) //如果X大於等於10，且X小於等於100，且Y大於等於10，，且Y小於等於200時
+
+            {
+            printf("%d,%d在矩形內!!\n",X,Y);
+            printf("輸入0,0可退出程式。\n");
+            }
+    else
+            {
+            printf("%d,%d不在矩形內!!\n",X,Y);
+            printf("輸入0,0可退出程式。\n");
+            }
+        }
+
+printf("程式結束");
+return 0;
+}
