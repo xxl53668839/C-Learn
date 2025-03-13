6f1b74666eb89c66060c3e68f3a245220b6f652e
xxl53668829 <xxl53668829@gmail.com>
2025-03-13 18:24:14 +0800

    修正 AD.h 檔案中的註解格式；刪除 AIS-R3.c 檔案；調整 address.c 檔案中的指標註解格式；新增 f(x).c 檔案以實現多項式計算功能並加入用戶輸入

--- call/address.c
+++ call/address.c
@@ -2,7 +2,7 @@
     int main()
 {
     int nurse,val;
-    int *ptr;
+    int *ptr; 
     nurse=22;
     ptr=&nurse; // ptr = nurse 的地址，&nurse = nurse 的地址
     val=*ptr;   // val = ptr 指向的值，又即 nurse 的變數值
--- f(x)/f(x).c
+++ f(x)/f(x).c
@@ -1,12 +1,34 @@
 #include <stdio.h>
 #include <math.h>
 
-int f(int x)
+int f(int x,int a[],int n)//定義函數f(x,a,n)
     {
-        
-        int a[10];
-        int sum,i,x;
-
-        sum=sum+A[i]*pow(x,i);
-        return 
+        int sum=0;//sum初始化
+        int i;
+        for(i=0;i<n;i++)
+        {
+            sum=sum+a[i]*pow(x,i);//意思是sum+a陣列乘以x的i次方
+        }
+        return sum;//回傳sum，f(x,a,n)的值=sum
     }
+
+int main()
+{
+    int n,x,i;
+    int a[10];
+    printf("\n請輸入n值:");
+    scanf("%d",&n);
+
+    for (i=0;i<n;i++)
+    {
+        printf("\n請輸入陣列元素:");
+        scanf("%d",&a[i]);
+    }
+
+    printf("\n請輸入x值:");
+    scanf("%d",&x);
+
+    printf("\n計算結果f(x)=%d\n",f(x,a,n));//呼叫函數f(x,a,n)
+
+    return 0;
+}
