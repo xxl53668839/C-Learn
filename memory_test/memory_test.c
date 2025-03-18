5f9074a8bfa898e565db9adcd3bc2f5b4cba4f31
xxl53668829 <xxl53668829@gmail.com>
2025-03-18 11:45:20 +0800

    重構 memory_test.c 檔案，合併測試函數並新增動態記憶體分配功能

--- memory_test/memory_test.c
+++ memory_test/memory_test.c
@@ -4,51 +4,44 @@
 #define MEMORY_SIZE 16 //限制記憶體大小為16byte
 int MEMORY[MEMORY_SIZE]; //利用ARRY模擬建立一個記憶體
 
-void memory_test0() //記憶體寫入0x55測試
+void memory_test(int *memory, int bit_set)  
 {
+
+
     for (int i=0;i<MEMORY_SIZE;i++)
     {
-        MEMORY[i]=0x55;
+        MEMORY[i]=bit_set;
     }
 
     for (int i=0;i<MEMORY_SIZE;i++)
     {
-        if(MEMORY[i]==0x55)
+        if(MEMORY[i]==bit_set)
         {
-            printf("測試0測試成功! memory=%#x\n",MEMORY[i]);
+            printf("測試成功! memory=%#x\n",MEMORY[i]);
         }
         else
         {
-            printf("測試0測試失敗! memory=%#x\n",MEMORY[i]);
-        }
-    }
-}
-
-void memory_test1()
-{
-    for (int i=0;i<MEMORY_SIZE;i++)
-    {
-        MEMORY[i]=0xAA;
-    }
-
-    for (int i=0;i<MEMORY_SIZE;i++)
-    {
-        if(MEMORY[i]==0xAA)
-        {
-            printf("測試1測試成功! memory=%#x\n",MEMORY[i]);
-        }
-        else
-        {
-            printf("測試1測試失敗! memory=%#x\n",MEMORY[i]);
+            printf("測試失敗! memory=%#x\n",MEMORY[i]);
         }
     }
 }
 
 int main()
 {
-    printf("開始執行測試0，對記憶體寫入0x55\n");
-    memory_test0();
-    printf("\n開始執行測試1，對記憶體寫入0xAA\n");
-    memory_test1();
+    int *MEMORY = (int *)malloc(MEMORY_SIZE); //分配動態記憶體
+
+    if(MEMORY==NULL)
+    {
+        printf("記憶體分配失敗");
+        return 1;
+    }
+
+    printf("開始執行測試1，對記憶體寫入0x55\n");
+    memory_test(MEMORY,0x55);
+    printf("\n開始執行測試2，對記憶體寫入0xAA\n");
+    memory_test(MEMORY,0xAA);
+
+    free(MEMORY);
+    printf("\n記憶體已釋放\n");
     return 0;
 }
