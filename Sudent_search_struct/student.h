a1114b8728f9f297076f2a7e1f92793d1783b3bf
xxl53668829 <xxl53668829@gmail.com>
2025-03-29 20:43:30 +0800

    修改 student.h 檔案，新增註解以增強結構體的可讀性

--- Sudent_search_struct/student.h
+++ Sudent_search_struct/student.h
@@ -2,11 +2,11 @@
 #define NAME 50
 #define CLASS 3
 
-struct student
+struct student //student裡面有三個東西
 {
     char name[NAME];
     int ID;
     int grade[CLASS];
-};
+}; //記得這邊一定要分號;;;;;;;;;;;;;;;;;;
 
-struct student student;
+struct student student; //將struct改名為student.
